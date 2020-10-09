d228a946f497f57e549ec5090bfc276984981fc4
Hamza Alalach <hamzaalalach@gmail.com>
2020-10-09 23:27:11 +0100

    Add basic structure

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const logger = require('morgan');
+const helmet = require('helmet');
+const compression = require('compression');
+const xss = require('xss-clean');
+const cors = require('cors');
+
+app.use(cors());
+app.use(compression());
+app.use(helmet());
+app.use(xss());
+app.use(logger(':method :url :status :response-time ms :remote-addr'));
+app.use(express.json());
+
+app.use('/api', require('./routes/api'));
+
+module.exports = app;
---
+++ routes/api.js
@@ -0,0 +1,5 @@
+const router = require('express').Router();
+
+router.get('/justify', (req, res) => {});
+
+module.exports = router;
